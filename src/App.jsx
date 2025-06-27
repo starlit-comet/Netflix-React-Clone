eca72f7647f084dd6810e710ca33209290234cbb
starlit-comet <3akshaiachu3@gmail.com>
2025-06-27 22:54:52 +0530

    completed home page design, started routing, added login & sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Home from './pages/Home/Home'
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-
+import Login from './pages/Login/Login.jsx'
 function App() {
 
   return (
     <div>
       <Routes>
       <Route path='/' element={<Home/>} />
+      < Route path='/login' element={<Login />} />
       </Routes>
     </div>
   )
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,24 +1,40 @@
-import React,{useRef,useEffect} from 'react'
+import React,{useRef,useEffect, useState} from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = ({title,category}) => {
+  const [apiData,setApiData] = useState([])
   const cardsRef = useRef()
+  // tmdb fetch options code from api documentation
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDZiNjRhM2UwZTczODIzOTI5ZGFlNDczMzNkNmNkZiIsIm5iZiI6MTc1MTAzMjQ1Ny4yMTYsInN1YiI6IjY4NWVhMjg5N2Q0NDg5OTljMjRkMTYwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.weTNDo2RvRjpcx7nwxzekFYiWJ4i1BqTCYGoIXGhFIs'
+  }
+};
+
   const handleWheel=(event)=>{
     event.preventDefault()
     cardsRef.current.scrollLeft += event.deltaY;
 
   }
   useEffect(()=>{
+    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel',handleWheel)
   },[])
   return (
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list " ref={cardsRef}>
-        {cards_data.map((card,index)=>{
+        {apiData.map((card,index)=>{
           return <div className="card" key={index}>
-              <img src={card.image} alt={`${card.name} poster`} />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500/${card.backdrop_path}.jpg`} alt={`${card.original_title} poster`} />
+              <p>{card.original_title}</p>
           </div>
         })}
       </div>
